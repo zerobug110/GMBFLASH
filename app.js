3e51aff255179b37b6f5d98b512a9daf61060d6f
zerobug110 <mahabojang110@gmail.com>
2020-09-09 01:03:05 +0000

    added a hamburger menu

---
+++ app.js
@@ -0,0 +1,6 @@
+const sideBar = document.querySelector('.side-bar');
+const lines = document.querySelector('.lines');
+
+lines.addEventListener('click', () => {
+    sideBar.classList.toggle('open');
+})
