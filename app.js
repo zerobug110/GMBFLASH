80f0141c0effc7ddde408880f1189c66687958bf
zerobug110 <mahabojang110@gmail.com>
2020-09-25 00:04:08 +0000

    added date generator to the footer

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
+//variables
 const sideBar = document.querySelector('.side-bar');
 const lines = document.querySelector('.lines');
+const date = document.getElementById('date')
 
+//eventListeners
 lines.addEventListener('click', () => {
     sideBar.classList.toggle('open');
 });
+
+//generate the current year
+
+date.innerHTML = new Date().getFullYear();
+
 
